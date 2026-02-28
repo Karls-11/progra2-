675c3570fd1aa55c746c7ac39b20aaa1d6031d3f
Karls-11 <clopezr52@miumg.edu.gt>
2026-02-27 20:47:58 -0600

    Add files via upload

---
+++ TareaNodos.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+struct Nodo{
+	int dato;
+	Nodo* izq;
+	Nodo* der;
+};
+
+
+
+int main(){
+	
+	Nodo* raiz = NULL;
+	Nodo *nodo1, *nodo2, *nodo3, *nodo4, *nodo5, *nodo6, *nodo7, *nodo8;
+	
+	nodo1 = new Nodo;
+	nodo2 = new Nodo;
+	nodo3 = new Nodo;
+	nodo4 = new Nodo;
+	nodo5 = new Nodo;
+	nodo6 = new Nodo;
+	nodo7 = new Nodo;
+	nodo8 = new Nodo;
+	
+	nodo1->dato = 20;
+	nodo2->dato = 10;
+	nodo3->dato = 30;
+	nodo4->dato = 5;
+	nodo5->dato = 15;
+	nodo6->dato = 40;
+	nodo7->dato = 12;
+	nodo8->dato = 11;
+	
+	raiz = nodo1;
+	nodo1->izq = nodo2;
+	nodo1->der = nodo3;
+	nodo2->izq = nodo4;
+	nodo2->der = nodo5;
+	nodo3->izq = NULL;
+	nodo3->der = nodo6;
+	nodo4->izq = NULL;
+	nodo4->izq = NULL;
+	nodo5->izq = nodo7;
+	nodo5->der = NULL;
+	nodo6->izq = NULL;
+	nodo6->der = NULL;
+	nodo7->izq = nodo8;
+	nodo7->der = NULL;
+	nodo8->izq = NULL;
+	nodo8->der = NULL;
+	
+	
+	return 0;
+}
+
