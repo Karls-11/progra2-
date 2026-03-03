3087a59d97242af8586532c2d5c827095bdaa638
Karls-11 <clopezr52@miumg.edu.gt>
2026-03-03 01:02:29 -0600

    Add files via upload

--- TareaNodos.cpp
+++ TareaNodos.cpp
@@ -8,6 +8,54 @@ struct Nodo{
 };
 
 
+void preorden(Nodo * raiz){
+	if(raiz == NULL){
+		return;	
+	}
+	
+	cout<<raiz->dato<<",";
+	preorden(raiz->izq);
+	preorden(raiz->der);
+}
+
+void inorden(Nodo * raiz){
+	if(raiz == NULL){
+		return;
+	}
+	
+	inorden(raiz->izq);
+	cout<<raiz->dato<<",";
+	inorden(raiz->der);
+}
+
+void postorden(Nodo * raiz){
+	if(raiz == NULL){
+		return;
+	}
+	
+	postorden(raiz->izq);
+	postorden(raiz->der);
+	cout<<raiz->dato<<",";
+}
+
+int totalnodos(Nodo* nodo){
+	if(nodo == NULL){
+		return 0;
+	}
+	return 1 + totalnodos(nodo->izq) + totalnodos(nodo->der);
+}
+
+int Hojas(Nodo* nodo){
+	if(nodo == NULL){
+		return 0;
+	}
+	
+	if(nodo->izq == NULL && nodo->der == NULL){
+		return 1;
+	}
+	return Hojas(nodo->izq)+Hojas(nodo->der);
+}
+
 
 int main(){
 	
@@ -40,7 +88,7 @@ int main(){
 	nodo3->izq = NULL;
 	nodo3->der = nodo6;
 	nodo4->izq = NULL;
-	nodo4->izq = NULL;
+	nodo4->der = NULL;
 	nodo5->izq = nodo7;
 	nodo5->der = NULL;
 	nodo6->izq = NULL;
@@ -50,7 +98,18 @@ int main(){
 	nodo8->izq = NULL;
 	nodo8->der = NULL;
 	
+	cout<<"Preorden: ";
+	preorden(raiz);
+	cout<<endl;
+	cout<<"Inorden: ";
+	inorden(raiz);
+	cout<<endl;
+	cout<<"Postorden: ";
+	postorden(raiz);
+	cout<<endl;
 	
+	cout<<"Total de nodos: "<<totalnodos(raiz)<<endl;
+	cout<<"Total de nodos hoja: "<<Hojas(raiz)<<endl;
 	return 0;
 }
 
